9d78d4286ce7e5faf95d2ea3ba6c7a30df876b1a
Michael <7471919@naver.com>
2023-06-06 17:37:17 +0900

    test: MainPostingCategory Test todo

---
+++ server/src/test/unit/main-posting-category.spec.ts
@@ -0,0 +1,17 @@
+describe('MainPostingCategory Spec', () => {
+  describe('1. Create Category', () => {
+    it.todo('1-1. 카테고리를 생성합니다.');
+  });
+
+  describe('2. Update Category', () => {
+    it.todo('2-1. 카테고리를 수정합니다.');
+  });
+
+  describe('3. Delete Category', () => {
+    it.todo('3-1. 카테고리를 삭제합니다.');
+  });
+
+  describe('4. Read Category List', () => {
+    it.todo('4-1. 카테고리 목록을 불러옵니다.');
+  });
+});
