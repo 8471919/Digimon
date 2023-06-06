c700ed316cba3f9928e6ef02b6f7cab0054c7f3a
Michael <7471919@naver.com>
2023-06-06 22:00:34 +0900

    fix: wrong DI for AdminRepository

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,6 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import {
+  ADMIN_REPOSITORY_OUTBOUND_PORT,
+  AdminRepositoryOutboundPort,
+} from 'src/database/repositories/outbound-ports/admin-repository.outbound-port';
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(
+    @Inject(ADMIN_REPOSITORY_OUTBOUND_PORT)
+    private readonly adminRepo: AdminRepositoryOutboundPort,
+  ) {}
 }
--- server/src/database/repositories/admin.repository.ts
+++ server/src/database/repositories/admin.repository.ts
@@ -1,14 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
-import {
-  ADMIN_REPOSITORY_OUTBOUND_PORT,
-  AdminRepositoryOutboundPort,
-} from './outbound-ports/admin-repository.outbound-port';
+import { AdminRepositoryOutboundPort } from './outbound-ports/admin-repository.outbound-port';
 
 @Injectable()
 export class AdminRepository implements AdminRepositoryOutboundPort {
-  constructor(
-    @Inject(ADMIN_REPOSITORY_OUTBOUND_PORT)
-    private readonly adminRepo: AdminRepositoryOutboundPort,
-  ) {}
+  constructor(private readonly prisma: PrismaService) {}
 }
