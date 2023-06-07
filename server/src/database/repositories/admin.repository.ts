e99d0eb6c9fb775193ea4fbf2be35c8b8e4fbe86
Michael <7471919@naver.com>
2023-06-08 01:14:12 +0900

    feat: findOneAdminForSignUp function in AdminRepository

--- server/src/database/repositories/admin.repository.ts
+++ server/src/database/repositories/admin.repository.ts
@@ -5,10 +5,19 @@ import { AdminOptionsDto } from '../dtos/admin/admin-options.dto';
 import { FindOneAdminByOptionsOutputDto } from '../dtos/admin/admin.outbound-port.dto';
 import typia from 'typia';
 import { TypeToSelect } from 'src/utils/types/type-to-select.type';
+import { Admin } from '@prisma/client';
 
 @Injectable()
 export class AdminRepository implements AdminRepositoryOutboundPort {
   constructor(private readonly prisma: PrismaService) {}
+
+  async findOneAdminForSignUp(email: string): Promise<Admin | null> {
+    const admin = await this.prisma.admin.findFirst({
+      where: { email },
+    });
+
+    return admin;
+  }
 
   async findOneAdminByOptions(
     options: AdminOptionsDto,
