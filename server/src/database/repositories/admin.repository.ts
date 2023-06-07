f7a2d92479cac9f5ddd0a04094ecfcbba3a54815
Michael <7471919@naver.com>
2023-06-07 22:52:27 +0900

    feat: findOneAdminByOptions function

--- server/src/database/repositories/admin.repository.ts
+++ server/src/database/repositories/admin.repository.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 import { AdminRepositoryOutboundPort } from './outbound-ports/admin-repository.outbound-port';
+import { AdminOptionsDto } from '../dtos/admin/admin-options.dto';
+import { FindOneAdminByOptionsOutputDto } from '../dtos/admin/admin.outbound-port.dto';
+import typia from 'typia';
+import { TypeToSelect } from 'src/utils/types/type-to-select.type';
 
 @Injectable()
 export class AdminRepository implements AdminRepositoryOutboundPort {
   constructor(private readonly prisma: PrismaService) {}
+
+  async findOneAdminByOptions(
+    options: AdminOptionsDto,
+  ): Promise<FindOneAdminByOptionsOutputDto | null> {
+    const admin = await this.prisma.admin.findFirst({
+      select: typia.random<TypeToSelect<FindOneAdminByOptionsOutputDto>>(),
+      where: options,
+    });
+
+    return admin;
+  }
 }
