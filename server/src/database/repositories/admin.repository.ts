31de54394c815b9c7ba0c2996878d341280dff0f
Michael <7471919@naver.com>
2023-06-08 15:28:04 +0900

    fix: modify function name findOneAdminByOptionsOutputDto to findOneAdminExceptPasswordDto

--- server/src/database/repositories/admin.repository.ts
+++ server/src/database/repositories/admin.repository.ts
@@ -2,14 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 import { AdminRepositoryOutboundPort } from './outbound-ports/admin-repository.outbound-port';
 import { AdminOptionsDto } from '../dtos/admin/admin-options.dto';
-import { FindOneAdminByOptionsOutputDto } from '../dtos/admin/admin.outbound-port.dto';
+import { FindOneAdminExceptPasswordDto } from '../dtos/admin/admin.outbound-port.dto';
 import typia from 'typia';
 import { TypeToSelect } from 'src/utils/types/type-to-select.type';
 import { Admin } from '@prisma/client';
+import { AdminSignUpInputDto } from '../dtos/admin/admin.inbound-port.dto';
 
 @Injectable()
 export class AdminRepository implements AdminRepositoryOutboundPort {
   constructor(private readonly prisma: PrismaService) {}
+
+  async insertAdmin(
+    adminInfo: AdminSignUpInputDto,
+  ): Promise<FindOneAdminExceptPasswordDto | null> {
+    const admin = await this.prisma.admin.create({
+      data: adminInfo,
+      select: typia.random<TypeToSelect<FindOneAdminExceptPasswordDto>>(),
+    });
+
+    return admin;
+  }
 
   async findOneAdminForSign(email: string): Promise<Admin | null> {
     const admin = await this.prisma.admin.findFirst({
@@ -21,9 +33,9 @@ export class AdminRepository implements AdminRepositoryOutboundPort {
 
   async findOneAdminByOptions(
     options: AdminOptionsDto,
-  ): Promise<FindOneAdminByOptionsOutputDto | null> {
+  ): Promise<FindOneAdminExceptPasswordDto | null> {
     const admin = await this.prisma.admin.findFirst({
-      select: typia.random<TypeToSelect<FindOneAdminByOptionsOutputDto>>(),
+      select: typia.random<TypeToSelect<FindOneAdminExceptPasswordDto>>(),
       where: options,
     });
 
--- server/src/database/repositories/outbound-ports/admin-repository.outbound-port.ts
+++ server/src/database/repositories/outbound-ports/admin-repository.outbound-port.ts
@@ -1,14 +1,19 @@
 import { Admin } from '@prisma/client';
 import { AdminOptionsDto } from 'src/database/dtos/admin/admin-options.dto';
-import { FindOneAdminByOptionsOutputDto } from 'src/database/dtos/admin/admin.outbound-port.dto';
+import { AdminSignUpInputDto } from 'src/database/dtos/admin/admin.inbound-port.dto';
+import { FindOneAdminExceptPasswordDto } from 'src/database/dtos/admin/admin.outbound-port.dto';
 
 export const ADMIN_REPOSITORY_OUTBOUND_PORT =
   'ADMIN_REPOSITORY_OUTBOUND_PORT' as const;
 
 export interface AdminRepositoryOutboundPort {
+  insertAdmin(
+    adminInfo: AdminSignUpInputDto,
+  ): Promise<FindOneAdminExceptPasswordDto | null>;
+
   findOneAdminForSign(email: string): Promise<Admin | null>;
 
   findOneAdminByOptions(
     options: AdminOptionsDto,
-  ): Promise<FindOneAdminByOptionsOutputDto | null>;
+  ): Promise<FindOneAdminExceptPasswordDto | null>;
 }
