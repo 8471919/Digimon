fbf9cf075d11d1521d398610cc336584ee99e267
Michael <7471919@naver.com>
2023-06-08 17:26:05 +0900

    fix: adminSignIn to use adinSignIn of AuthService

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,10 +1,9 @@
 import { Controller, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { TypedBody, TypedRoute } from '@nestia/core';
+import { TypedRoute } from '@nestia/core';
 import { AdminGuard } from './guards/admin.guard';
 import { User } from 'src/common/decorators/user.decorator';
-import { FindOneAdminExceptPasswordDto } from 'src/database/dtos/admin/admin.outbound-port.dto';
-import { AdminSignUpInputDto } from 'src/database/dtos/admin/admin.inbound-port.dto';
+import { AdminLogInDto } from 'src/database/dtos/auth/admin-login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,9 +11,11 @@ export class AuthController {
 
   @UseGuards(AdminGuard)
   @TypedRoute.Post('sign-in')
-  async adminSignIn(@User() user) {
+  async adminSignIn(@User() user: AdminLogInDto) {
     console.log(user);
-    return user;
+    const token = await this.authService.adminSignIn(user);
+
+    return token;
   }
 
   // @TypedRoute.Post('master-sign-up')
