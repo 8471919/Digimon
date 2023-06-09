af302bae86586b0945f0dea3083c72f45d1f0a78
Michael <7471919@naver.com>
2023-06-09 23:04:53 +0900

    fix: fix comment in admin spec

--- server/src/test/unit/admin.spec.ts
+++ server/src/test/unit/admin.spec.ts
@@ -8,9 +8,11 @@ import { AdminLogInDto } from 'src/database/dtos/auth/admin-login.dto';
 import { AdminRepositoryOutboundPort } from 'src/database/repositories/outbound-ports/admin-repository.outbound-port';
 import typia from 'typia';
 
-// 동일한 repository 함수를 두 번 호출할 경우, 결과 값을 1개만 쓸수 밖에 없다는 단점이 있다.
-// 그래서 차라리 Array형식으로 만들어서 함수 호출할 때 마다, pop을 하는 형식으로 하도록 만들었다.
-// 어차피, pop으로 변수에 한 번 할당하면, 해당 변수를 이용해서 동일한 값을 재사용 할 수 있으니까.
+/**
+ * 동일한 repository 함수를 두 번 호출할 경우, 결과 값을 1개만 쓸수 밖에 없다는 단점이 있다.
+ * 그래서 차라리 Array형식으로 만들어서 함수 호출할 때 마다, pop을 하는 형식으로 하도록 만들었다.
+ * 어차피, pop으로 변수에 한 번 할당하면, 해당 변수를 이용해서 동일한 값을 재사용 할 수 있으니까.
+ */
 type MockAdminRepositoryParamType = {
   inserAdmin?: Array<FindOneAdminExceptPasswordDto>;
   findOneAdminForSign?: Array<Admin | null>;
