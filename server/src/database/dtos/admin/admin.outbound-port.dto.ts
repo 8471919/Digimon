1c4d6e05797ea43dbbebed94490831dfafb2402e
Michael <7471919@naver.com>
2023-06-16 03:00:31 +0900

    chore: FindAdminInfoForCommonDto

--- server/src/database/dtos/admin/admin.outbound-port.dto.ts
+++ server/src/database/dtos/admin/admin.outbound-port.dto.ts
@@ -1,6 +1,11 @@
-import { Admin } from '@prisma/client';
+import { Admin, MainPosting } from '@prisma/client';
 import { DateKeyToString } from 'src/utils/types/date-to-string.type';
 
 export type FindOneAdminExceptPasswordDto = DateKeyToString<
   Omit<Admin, 'password'>
 >;
+
+export interface FindAdminInfoForCommonDto
+  extends Pick<Admin, 'id' | 'nickname' | 'introduction'> {
+  mainPostings: DateKeyToString<MainPosting>[];
+}
