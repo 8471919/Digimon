a01d4c114fbd7ad2da8515720dd9cc21fac2d1aa
Michael <7471919@naver.com>
2023-06-16 02:55:21 +0900

    fix: modify DateKeyToString type

--- server/src/utils/types/date-to-string.type.ts
+++ server/src/utils/types/date-to-string.type.ts
@@ -1,5 +1,9 @@
 type DateToString<T> = T extends Date ? string : T;
 
 export type DateKeyToString<T extends object> = {
-  [P in keyof T]: DateToString<T[P]>;
+  [P in keyof T]: DateToString<T[P]> extends T[P]
+    ? T[P] extends object
+      ? DateKeyToString<T[P]>
+      : T[P]
+    : DateToString<T[P]>;
 };
