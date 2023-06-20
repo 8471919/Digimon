ea4fd7ee342eb99af5a5660037098db510124c07
Michael <7471919@naver.com>
2023-06-20 21:01:54 +0900

    chore: create crud-bool.dto

---
+++ server/src/database/dtos/common/crud-bool.dto.ts
@@ -0,0 +1,15 @@
+export type IsCreatedOutputDto = {
+  created: boolean;
+};
+
+export type IsReadOutputDto = {
+  read: boolean;
+};
+
+export type IsUpdatedOutputDto = {
+  updated: boolean;
+};
+
+export type IsDeletedOutputDto = {
+  deleted: boolean;
+};
