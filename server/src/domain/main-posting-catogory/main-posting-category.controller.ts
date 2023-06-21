d5b1ff18746374156b48cc5608059a8f4f3a9130
Michael <7471919@naver.com>
2023-06-21 17:56:36 +0900

    feat: saveCategoryChanges

--- server/src/domain/main-posting-catogory/main-posting-category.controller.ts
+++ server/src/domain/main-posting-catogory/main-posting-category.controller.ts
@@ -1,9 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { MainPostingCategoryService } from './main-posting-category.service';
+import { TypedBody, TypedRoute } from '@nestia/core';
+import { JwtMasterAdminGuard } from 'src/auth/guards/jwt-master-admin.guard';
+import { User } from 'src/common/decorators/user.decorator';
+import { AdminLogInDto } from 'src/database/dtos/auth/admin-login.dto';
+import { ADMIN_GRADE } from 'src/database/values/admin-grade.value';
+import {
+  SaveChangesMainPostingCategoryInputDto,
+  SaveChangesMainPostingCategoryOutputDto,
+} from 'src/database/dtos/main-posting-category/main-posting-category.outbount-port.dto';
 
 @Controller('api/v1/main-posting-category')
 export class MainPostingCategoryController {
   constructor(
     private readonly mainPostingCategoryService: MainPostingCategoryService,
   ) {}
+
+  @UseGuards(JwtMasterAdminGuard)
+  @TypedRoute.Put()
+  async saveCategoryChanges(
+    @TypedBody() body: SaveChangesMainPostingCategoryInputDto,
+    @User() user: AdminLogInDto,
+  ): Promise<SaveChangesMainPostingCategoryOutputDto> {
+    if (user.gradeId !== ADMIN_GRADE.master) {
+      throw new UnauthorizedException('UnAuthorized');
+    }
+
+    const res = this.mainPostingCategoryService.saveCategoryChanges(body);
+
+    return res;
+  }
 }
--- server/src/domain/main-posting-catogory/main-posting-category.service.ts
+++ server/src/domain/main-posting-catogory/main-posting-category.service.ts
@@ -1,5 +1,9 @@
 import { Inject, Injectable } from '@nestjs/common';
 import {
+  SaveChangesMainPostingCategoryInputDto,
+  SaveChangesMainPostingCategoryOutputDto,
+} from 'src/database/dtos/main-posting-category/main-posting-category.outbount-port.dto';
+import {
   MAIN_POSTING_CATEGORY_REPOSITORY_OUTBOUND_PORT,
   MainPostingCategoryRepositoryOutbountPort,
 } from 'src/database/repositories/outbound-ports/main-posting-category-repository.outbound-port';
@@ -10,4 +14,12 @@ export class MainPostingCategoryService {
     @Inject(MAIN_POSTING_CATEGORY_REPOSITORY_OUTBOUND_PORT)
     private readonly mainPostingCategoryRepo: MainPostingCategoryRepositoryOutbountPort,
   ) {}
+
+  async saveCategoryChanges(
+    data: SaveChangesMainPostingCategoryInputDto,
+  ): Promise<SaveChangesMainPostingCategoryOutputDto> {
+    const res = this.mainPostingCategoryRepo.saveChanges(data);
+
+    return res;
+  }
 }
