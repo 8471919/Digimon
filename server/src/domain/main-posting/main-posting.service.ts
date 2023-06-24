49177831a86b285db8ba1f4c0c82b74b6a8736d2
Michael <7471919@naver.com>
2023-06-24 18:24:39 +0900

    feat: modifyMainPosting in MainPostingService

--- server/src/domain/main-posting/main-posting.service.ts
+++ server/src/domain/main-posting/main-posting.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateMainPostingInputDto } from 'src/database/dtos/main-posting/main-posting.outbound-port.dto';
 import { MainPostingEntity } from 'src/database/models/main-posting/main-posting.entity';
 import {
@@ -19,6 +24,32 @@ export class MainPostingService {
   ): Promise<MainPostingEntity.MainPosting> {
     const mainPosting = await this.mainPostingRepo.insertMainPosting(
       adminId,
+      data,
+    );
+
+    if (!mainPosting) {
+      throw new BadRequestException('Incorrect option');
+    }
+
+    return mainPosting;
+  }
+
+  async modifyMainPosting(
+    mainPostingId: string,
+    adminId: number,
+    data: CreateMainPostingInputDto,
+  ): Promise<MainPostingEntity.MainPosting> {
+    const isFound = await this.mainPostingRepo.findMainPosting({
+      id: mainPostingId,
+      adminId,
+    });
+
+    if (!isFound) {
+      throw new UnauthorizedException("You're not Author");
+    }
+
+    const mainPosting = await this.mainPostingRepo.updateMainPosting(
+      mainPostingId,
       data,
     );
 
