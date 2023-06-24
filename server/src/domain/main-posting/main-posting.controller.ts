81622d2da0bc66a75385cf7ee5dafcb9f2fcf9f2
Michael <7471919@naver.com>
2023-06-24 16:37:47 +0900

    feat: createMainPosting in MainPostingController

--- server/src/domain/main-posting/main-posting.controller.ts
+++ server/src/domain/main-posting/main-posting.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { MainPostingService } from './main-posting.service';
+import { TypedBody, TypedRoute } from '@nestia/core';
+import { JwtAdminGuard } from 'src/auth/guards/jwt-admin.guard';
+import { User } from 'src/common/decorators/user.decorator';
+import { AdminLogInDto } from 'src/database/dtos/auth/admin-login.dto';
+import { CreateMainPostingInputDto } from 'src/database/dtos/main-posting/main-posting.outbound-port.dto';
 
 @Controller('api/v1/main-posting')
 export class MainPostingController {
   constructor(private readonly mainPostingService: MainPostingService) {}
+
+  @UseGuards(JwtAdminGuard)
+  @TypedRoute.Post()
+  async createMainPosting(
+    @TypedBody() body: CreateMainPostingInputDto,
+    @User() user: AdminLogInDto,
+  ) {
+    const mainPosting = await this.mainPostingService.createMainPosting(
+      user.id,
+      {
+        ...body,
+      },
+    );
+
+    return mainPosting;
+  }
 }
