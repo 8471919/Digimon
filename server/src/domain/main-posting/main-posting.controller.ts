27de1f05dd793a2316773077dec0b809a5b34c66
Michael <7471919@naver.com>
2023-06-25 22:05:34 +0900

    feat: getMainPostingList in MainPostingController

--- server/src/domain/main-posting/main-posting.controller.ts
+++ server/src/domain/main-posting/main-posting.controller.ts
@@ -1,16 +1,47 @@
 import { Controller, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { MainPostingService } from './main-posting.service';
-import { TypedBody, TypedParam, TypedRoute } from '@nestia/core';
+import { TypedBody, TypedParam, TypedQuery, TypedRoute } from '@nestia/core';
 import { JwtAdminGuard } from 'src/auth/guards/jwt-admin.guard';
 import { User } from 'src/common/decorators/user.decorator';
 import { AdminLogInDto } from 'src/database/dtos/auth/admin-login.dto';
-import { CreateMainPostingInputDto } from 'src/database/dtos/main-posting/main-posting.outbound-port.dto';
+import {
+  CreateMainPostingInputDto,
+  FindMainPostingListOptionsForPagenationDto,
+} from 'src/database/dtos/main-posting/main-posting.outbound-port.dto';
 import { IsDeletedOutputDto } from 'src/database/dtos/common/crud-bool.dto';
 import { MainPostingEntity } from 'src/database/models/main-posting/main-posting.entity';
+import { MainPostingPagenationQueryInputDto } from 'src/database/dtos/main-posting/main-posting.inbound-port.dto';
 
 @Controller('api/v1/main-posting')
 export class MainPostingController {
   constructor(private readonly mainPostingService: MainPostingService) {}
+
+  @TypedRoute.Get('list')
+  async getMainPostingList(
+    @TypedQuery() query: MainPostingPagenationQueryInputDto,
+  ) {
+    const input: FindMainPostingListOptionsForPagenationDto = {
+      countPerPage: query.countPerPage,
+      pageNumber: query.pageNumber,
+      options: {
+        adminId: query.adminId,
+        categoryId: query.categoryId,
+      },
+      order:
+        query.orderType && query.order
+          ? {
+              type: query.orderType,
+              order: query.order,
+            }
+          : undefined,
+    };
+
+    const mainPostings = await this.mainPostingService.getMainPostingList(
+      input,
+    );
+
+    return mainPostings;
+  }
 
   @UseGuards(JwtAdminGuard)
   @TypedRoute.Post()
