5285e3cebfcce982aa96bcab537b05dd83d64a29
Michael <7471919@naver.com>
2023-06-26 15:14:15 +0900

    feat: add middleware not to find softDeleted column

--- server/src/database/prisma/prisma.service.ts
+++ server/src/database/prisma/prisma.service.ts
@@ -11,6 +11,21 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
 
   async onModuleInit() {
     await this.$connect();
+
+    this.$use(async (params, next) => {
+      if (
+        params.action === 'findFirst' ||
+        params.action === 'findMany' ||
+        params.action === 'findRaw' ||
+        params.action === 'findUnique'
+      ) {
+        console.log(params.args);
+        params.args.where['deletedAt'] = null;
+        console.log(params.args);
+      }
+
+      return next(params);
+    });
   }
 
   async enableShutdownHooks(app: INestApplication) {
